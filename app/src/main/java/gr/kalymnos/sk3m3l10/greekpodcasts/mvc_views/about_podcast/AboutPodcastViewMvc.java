f7841ad2c64c5beb2b79ac436474b17704dc438b
Tomas Michael <tomasmichael995@gmail.com>
2018-06-21 00:51:30 +0300

    Deleted -for now- all the unesecairy mvc_views and their adapters.

--- app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_views/about_podcast/AboutPodcastViewMvc.java
+++
@@ -1,8 +0,0 @@
-package gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.about_podcast;
-
-import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.ViewMvc;
-
-public interface AboutPodcastViewMvc extends ViewMvc {
-
-    void bindText(String text);
-}
