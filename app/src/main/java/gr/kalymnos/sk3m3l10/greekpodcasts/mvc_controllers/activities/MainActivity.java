6c37303ccd3ad0c5b9c57cd770cd195106d9d54c
Tomas Michael <tomasmichael995@gmail.com>
2018-06-21 00:22:23 +0300

    Refactor: Moved MainActivity into mvc_controllers/activities package.

--- app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_controllers/activities/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_controllers/activities/MainActivity.java
@@ -1,9 +1,13 @@
-package gr.kalymnos.sk3m3l10.greekpodcasts;
+package gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.main_screen.MainViewMvc;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MainViewMvc viewMvc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
