5b0002ad499fc15ad9d87e2842a555146a785391
Tomas Michael <tomasmichael995@gmail.com>
2018-06-21 20:49:59 +0300

    Fixed arguments for fragments in PodcastPagerAdapter.

--- app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_views/podcast_screen/PodcastPagerAdapter.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_views/podcast_screen/PodcastPagerAdapter.java
@@ -10,27 +10,49 @@ import static gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.podcast_screen.Podcas
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers.fragments.AboutPodcastFragment;
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers.fragments.AllEpisodesFragment;
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.main_screen.MainPagerAdapter;
+import gr.kalymnos.sk3m3l10.greekpodcasts.pojos.Episode;
+import gr.kalymnos.sk3m3l10.greekpodcasts.pojos.Podcast;
+import gr.kalymnos.sk3m3l10.greekpodcasts.pojos.Podcaster;
 
 public class PodcastPagerAdapter extends MainPagerAdapter {
 
+    private Bundle allEpisodesArgs, aboutPodcastArgs;
 
-    public PodcastPagerAdapter(FragmentManager fm, @NonNull Bundle framgnetArgs,@NonNull String... titles) {
+    public PodcastPagerAdapter(FragmentManager fm, @NonNull Bundle pagerArguments, @NonNull String... titles) {
         super(fm, titles);
-        this.fragmentArgs = framgnetArgs;
+        initializeFragmentsArgs(pagerArguments);
     }
-
-    private Bundle fragmentArgs;
 
     public Fragment[] getFragmentsArray() {
         if (fragmentsArray == null) {
             fragmentsArray = new Fragment[TAB_COUNT];
 
             fragmentsArray[0] = new AllEpisodesFragment();
-            fragmentsArray[0].setArguments(this.fragmentArgs);
+            fragmentsArray[0].setArguments(allEpisodesArgs);
 
             fragmentsArray[1] = new AboutPodcastFragment();
-            fragmentsArray[1].setArguments(this.fragmentArgs);
+            fragmentsArray[1].setArguments(aboutPodcastArgs);
         }
         return fragmentsArray;
+    }
+
+    private void initializeFragmentsArgs(@NonNull Bundle pagerArguments) {
+        if (pagerArguments!=null && pagerArguments.containsKey(Podcast.PODCAST_KEY)){
+
+            Podcast podcast = pagerArguments.getParcelable(Podcast.PODCAST_KEY);
+
+            String posterUrl = podcast.getPosterUrl();
+            String podcasterId = podcast.getPodcasterId();
+            String episodesId = podcast.getEpisodesId();
+            allEpisodesArgs = new Bundle();
+            allEpisodesArgs.putString(Podcast.POSTER_KEY, posterUrl);
+            allEpisodesArgs.putString(Podcaster.PUSH_ID_KEY, podcasterId);
+            allEpisodesArgs.putString(Episode.EPISODES_KEY, episodesId);
+
+            aboutPodcastArgs = new Bundle();
+            aboutPodcastArgs.putString(Podcast.DESCRIPTION_KEY, podcast.getDescription());
+        }else {
+            throw new IllegalArgumentException(PodcastPagerAdapter.class.getSimpleName()+": Bundle is null or does not contain Podcast.PODCAST_KEY");
+        }
     }
 }
