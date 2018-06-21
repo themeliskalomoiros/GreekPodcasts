03f93c9b10f5bbb21778148c54fdb5963cb5cbfa
Tomas Michael <tomasmichael995@gmail.com>
2018-06-21 23:34:44 +0300

    Copied FragmentUtils.java.

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/utils/FragmentUtils.java
@@ -0,0 +1,16 @@
+package gr.kalymnos.sk3m3l10.greekpodcasts.utils;
+
+import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.ViewMvc;
+
+public class FragmentUtils {
+
+    private FragmentUtils() {
+    }
+
+    public static String episodesFragmentTag(int viewpagerId, int currentItem) {
+        //  Hack for getting tag of a fragment held by ViewPager
+        //  According to https://learnpainless.com/android/how-to-get-fragment-from-viewpager-android
+        //  This hack works only if ViewPagerAdapter extends FragmentPagerAdapter and not FragmentStatePagerAdapter
+        return String.format("android:switcher:%d:%d", viewpagerId, currentItem);
+    }
+}
