0940090b2e4f8dd398c7acf0573cb1bc0afb4694
Tomas Michael <tomasmichael995@gmail.com>
2018-07-02 22:15:56 +0300

    Bug fix: PortofolioPagerAdapter titles array was null.

--- app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_views/portofolio_screen/PortofolioPagerAdapter.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_views/portofolio_screen/PortofolioPagerAdapter.java
@@ -1,28 +1,57 @@
 package gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.portofolio_screen;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers.fragments.PortofolioCreateFragment;
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers.fragments.PortofolioPersonalFragment;
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers.fragments.PortofolioPublishFragment;
-import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.main_screen.MainPagerAdapter;
 
-public class PortofolioPagerAdapter extends MainPagerAdapter {
+public class PortofolioPagerAdapter extends FragmentPagerAdapter {
+
+    private static final String TAG = PortofolioPagerAdapter.class.getSimpleName();
+    private Fragment[] fragmentArray;
+    private String[] titlesArray;
 
     public PortofolioPagerAdapter(FragmentManager fm, @NonNull String... titles) {
-        super(fm, titles);
+        super(fm);
+        titlesArray = titles;
+    }
+
+    public Fragment[] getFragmentsArray() {
+        if (fragmentArray == null) {
+            initializeFragmentArray();
+        }
+        return fragmentArray;
+    }
+
+    private void initializeFragmentArray() {
+        fragmentArray = new Fragment[PortofolioViewMvc.TAB_COUNT];
+        fragmentArray[0] = new PortofolioCreateFragment();
+        fragmentArray[1] = new PortofolioPublishFragment();
+        fragmentArray[2] = new PortofolioPersonalFragment();
     }
 
     @Override
-    public Fragment[] getFragmentsArray() {
-        if (fragmentsArray == null) {
-            Fragment[] fragments = new Fragment[PortofolioViewMvc.TAB_COUNT];
-            fragments[0] = new PortofolioCreateFragment();
-            fragments[1] = new PortofolioPublishFragment();
-            fragments[2] = new PortofolioPersonalFragment();
+    public int getCount() {
+        return getFragmentsArray().length;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (titlesArray != null && titlesArray.length == PortofolioViewMvc.TAB_COUNT) {
+            return titlesArray[position];
+        }else{
+            throw new UnsupportedOperationException(TAG+": title array is null or length less than "+PortofolioViewMvc.TAB_COUNT);
         }
-        return fragmentsArray;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return getFragmentsArray()[position];
     }
 }
