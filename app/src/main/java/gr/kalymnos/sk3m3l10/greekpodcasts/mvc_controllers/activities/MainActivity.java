4f9f74efe082926148cb994864afd7c691caf212
Tomas Michael <tomasmichael995@gmail.com>
2018-07-02 15:07:47 +0300

    Configured Firebase.

--- app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_controllers/activities/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_controllers/activities/MainActivity.java
@@ -1,16 +1,11 @@
 package gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers.activities;
 
-import android.database.Cursor;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.widget.Toast;
 
-import java.io.File;
+import com.google.firebase.database.FirebaseDatabase;
 
-import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_model.local_database.UserMetadataContract;
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.main_screen.MainViewMvc;
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.main_screen.MainViewMvcImpl;
 
@@ -30,18 +25,7 @@ public class MainActivity extends AppCompatActivity implements MainViewMvc.OnAct
 
     @Override
     public void onActionCreatePodcastClick() {
-        //  TODO:   Implement the create podcast action
-        Cursor cursor = getContentResolver().query(UserMetadataContract.EpisodeEntry.CONTENT_URI,
-                null,null,null,null);
-
-        while (cursor.moveToNext()){
-            int idIndex = cursor.getColumnIndex(UserMetadataContract.EpisodeEntry._ID);
-            int uriIndex = cursor.getColumnIndex(UserMetadataContract.EpisodeEntry.COLUMN_NAME_DOWNLOADED_URI);
-            String string = String.format("Episode with id %d and uri %s",cursor.getInt(idIndex),cursor.getString(uriIndex));
-            Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
-        }
-
-        cursor.close();
+        FirebaseDatabase.getInstance().getReference().setValue("ela ");
     }
 
     private void initializeViewMvc() {
