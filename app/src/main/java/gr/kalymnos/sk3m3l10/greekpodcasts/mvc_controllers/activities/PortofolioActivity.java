2667c9c468e56ee71f6779108e9071ced1567df4
Tomas Michael <tomasmichael995@gmail.com>
2018-07-02 21:21:09 +0300

    Created PortofolioActivity.

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_controllers/activities/PortofolioActivity.java
@@ -0,0 +1,22 @@
+package gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.portofolio_screen.PortofolioViewMvc;
+import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.portofolio_screen.PortofolioViewMvcImpl;
+
+public class PortofolioActivity extends AppCompatActivity {
+
+    private PortofolioViewMvc viewMvc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewMvc = new PortofolioViewMvcImpl(LayoutInflater.from(this), null, this.getSupportFragmentManager());
+        setContentView(viewMvc.getRootView());
+    }
+
+
+}
