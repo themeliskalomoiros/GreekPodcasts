f802f516631b9793c92354e16dd468491b2b4c03
Tomas Michael <tomasmichael995@gmail.com>
2018-07-06 23:57:30 +0300

    Implemented onSaveClick() in PortofolioActivity.

--- app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_controllers/activities/PortofolioActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_controllers/activities/PortofolioActivity.java
@@ -1,23 +1,42 @@
 package gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers.activities;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.widget.Toast;
 
+import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_controllers.fragments.portofolio.ChangeSaver;
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.portofolio_screen.PortofolioViewMvc;
 import gr.kalymnos.sk3m3l10.greekpodcasts.mvc_views.portofolio_screen.PortofolioViewMvcImpl;
+import gr.kalymnos.sk3m3l10.greekpodcasts.utils.FragmentUtils;
 
-public class PortofolioActivity extends AppCompatActivity {
+public class PortofolioActivity extends AppCompatActivity implements PortofolioViewMvc.OnActionSaveClickListener {
 
+    private static final String TAG = PortofolioActivity.class.getSimpleName();
     private PortofolioViewMvc viewMvc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewMvc = new PortofolioViewMvcImpl(LayoutInflater.from(this), null, this.getSupportFragmentManager());
+        viewMvc.setOnActionSaveClickListener(this);
         setSupportActionBar(viewMvc.getToolbar());
         setContentView(viewMvc.getRootView());
     }
 
 
+    @Override
+    public void onSaveClick() {
+        String currentFragmentTag = FragmentUtils.fragmentTag(viewMvc.getViewPagerId(), viewMvc.getCurrentTabItem());
+        if (!TextUtils.isEmpty(currentFragmentTag)) {
+            ChangeSaver currentFragment = (ChangeSaver) getSupportFragmentManager().findFragmentByTag(currentFragmentTag);
+            if (currentFragment != null) {
+                currentFragment.save();
+            } else {
+                throw new UnsupportedOperationException(TAG + ": all PortofolioActivity's fragments must implement " + ChangeSaver.class.getSimpleName());
+            }
+        }
+    }
 }
