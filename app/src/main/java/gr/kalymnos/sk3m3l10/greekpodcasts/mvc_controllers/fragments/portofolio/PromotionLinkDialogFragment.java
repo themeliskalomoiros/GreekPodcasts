8d981646b1c074d5b1dd3b79a7ed06f7d7d596a4
Tomas Michael <tomasmichael995@gmail.com>
2018-07-12 20:46:46 +0300

    Created OnInsertedTextListener for PromotionLinkDialogFragment. The callback's method is called when the user hits the ok button of the promotion links dialog.

--- app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_controllers/fragments/portofolio/PromotionLinkDialogFragment.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/mvc_controllers/fragments/portofolio/PromotionLinkDialogFragment.java
@@ -6,21 +6,49 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import gr.kalymnos.sk3m3l10.greekpodcasts.R;
 
 public class PromotionLinkDialogFragment extends DialogFragment {
 
+    private EditText titleInput, urlInput;
+
+    interface OnInsertedTextListener {
+        void onInsertedText(String title, String url);
+    }
+
+    private OnInsertedTextListener callback;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.promotion_dialog, null))
-                .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+        return createDialog(initializeViews());
+    }
 
+    private Dialog createDialog(View root) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),R.style.InsertTextDialog);
+        builder.setView(root)
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                    if (callback != null) {
+                        callback.onInsertedText(titleInput.getText().toString(), urlInput.getText().toString());
+                    }
                 })
                 .setNegativeButton(android.R.string.cancel, null);
         return builder.create();
+    }
+
+    @NonNull
+    private View initializeViews() {
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View root = inflater.inflate(R.layout.promotion_dialog, null);
+        titleInput = root.findViewById(R.id.portofolio_title_edittext);
+        urlInput = root.findViewById(R.id.portofolio_url_edittext);
+        return root;
+    }
+
+    public void setOnInsertedTextListener(OnInsertedTextListener listener) {
+        this.callback = listener;
     }
 }
