a70e5990196c4a49151610657e294a9fd727aac1
Tomas Michael <tomasmichael995@gmail.com>
2018-07-28 08:12:46 +0300

    Created BitmapUtils.bitmapFromUrl.

--- app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/utils/BitmapUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/greekpodcasts/utils/BitmapUtils.java
@@ -2,14 +2,19 @@ package gr.kalymnos.sk3m3l10.greekpodcasts.utils;
 
 import android.content.ContentResolver;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.ImageView;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class BitmapUtils {
 
@@ -24,6 +29,19 @@ public class BitmapUtils {
         } catch (IOException e) {
             e.printStackTrace();
             Log.d(TAG, e.getMessage());
+            return null;
+        }
+    }
+
+    public static Bitmap bitmapFromUrl(@NonNull URL imageUrl) {
+        try {
+            URLConnection connection = imageUrl.openConnection();
+            connection.connect();
+            InputStream inputStream = connection.getInputStream();
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            return BitmapFactory.decodeStream(bufferedInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
             return null;
         }
     }
